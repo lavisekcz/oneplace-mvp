a6b84f731bed5497e3aab3d7f372d31cb3e91269
lavisekcz <lavisek0@gmail.com>
2025-07-10 19:07:09 +0200

    Add files via upload

---
+++ pages/index.tsx
@@ -0,0 +1,11 @@
+
+import React from 'react';
+
+export default function Home() {
+  return (
+    <div style={{ fontFamily: 'sans-serif', textAlign: 'center', paddingTop: '50px' }}>
+      <h1>Welcome to OnePlace MVP</h1>
+      <p>This is a minimal version ready for deployment.</p>
+    </div>
+  );
+}
