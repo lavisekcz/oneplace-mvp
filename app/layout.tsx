5150dcad2d1a94b0078567c7bdb1c9e269f5e06a
lavisekcz <lavisek0@gmail.com>
2025-07-11 16:13:01 +0200

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,15 @@
+<head>
+  ...
+  {(process.env.NODE_ENV === "development" || process.env.VERCEL_ENV === "preview") && (
+    // eslint-disable-next-line @next/next/no-sync-scripts
+    <script
+      data-recording-token="0jRcTZMmnA5ik4cBhMZlrILFUhE0MNQZQzru32nY"
+      data-is-production-environment="false"
+      src="https://snippet.meticulous.ai/v1/meticulous.js"
+    />
+  )}
+  ...
+</head>
 import './globals.css';
 import type { Metadata } from 'next';
 
