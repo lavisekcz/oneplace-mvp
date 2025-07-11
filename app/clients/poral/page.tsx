6b2feef59a27dbc40967e28c776bb729148f4787
lavisekcz <lavisek0@gmail.com>
2025-07-11 16:51:39 +0200

    Delete app/clients/poral directory

--- app/clients/poral/page.tsx
+++
@@ -1,9 +0,0 @@
-// app/clients/portal/page.tsx
-export default function ProfessionalsOverviewPage() {
-  return (
-    <div>
-      <h1>Přehled výhod pro Profíky</h1>
-      <p>Zde bude obsah o výhodách členství.</p>
-    </div>
-  );
-}
