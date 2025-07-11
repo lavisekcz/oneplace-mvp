0666a2a83af90560c1be6c24319a166b60f69ff1
lavisekcz <lavisek0@gmail.com>
2025-07-11 16:37:34 +0200

    Update page.tsx

---
+++ app/clients/page.tsx
@@ -0,0 +1,9 @@
+// app/cliens/page.tsx
+export default function ProfessionalsOverviewPage() {
+  return (
+    <div>
+      <h1>Přehled výhod pro Klienty</h1>
+      <p>Zde bude obsah o výhodách členství.</p>
+    </div>
+  );
+}
