6181f69b94802e9d0972a00bcd994deed966abbd
lavisekcz <lavisek0@gmail.com>
2025-07-11 16:34:55 +0200

    Update page.tsx

---
+++ app/professionals/membership/page.tsx
@@ -0,0 +1,9 @@
+// app/professionals/overview/page.tsx
+export default function ProfessionalsOverviewPage() {
+  return (
+    <div>
+      <h1>Přehled výhod pro Profíky</h1>
+      <p>Zde bude obsah o výhodách členství.</p>
+    </div>
+  );
+}
